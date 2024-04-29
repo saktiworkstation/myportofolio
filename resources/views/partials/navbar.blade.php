4f3bb6cc03e3b2db4ec7bf180e7d6846d9e5c4ab
saktiworkstation <saktiworkstation@gmail.com>
2024-04-29 20:59:23 +0700

    Create partial navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,48 @@
+<div class="container">
+    <header
+        class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div class="col-md-3 mb-2 mb-md-0">
+            E-TRACT
+        </div>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="/" class="nav-link px-2 link-secondary">Home</a></li>
+            <li><a href="#" class="nav-link px-2">Features</a></li>
+            <li><a href="/#tickets" class="nav-link px-2">Tickets</a></li>
+            <li><a href="#" class="nav-link px-2">FAQs</a></li>
+            <li><a href="#" class="nav-link px-2">About</a></li>
+        </ul>
+        @auth
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Welcome back, {{ auth()->user()->name }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-window-reverse"></i>
+                                Dashboard</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <form action="/logout" method="post">
+                                @csrf
+                                <button class="dropdown-item" type="submit">
+                                    <i class="bi bi-box-arrow-in-right"></i> Logout
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        @else
+            <div class="col-md-3 text-end">
+                <a type="button" href="/login" class="btn btn-outline-primary me-2">Login</a>
+                <a type="button" href="/register" class="btn btn-primary">Sign-up</a>
+            </div>
+        @endauth
+
+
+    </header>
+</div>
