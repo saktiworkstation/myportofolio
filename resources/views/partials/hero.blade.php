9b61be998e80afa56db9eeb166f1bb03fbb8fe98
saktiworkstation <saktiworkstation@gmail.com>
2024-04-29 21:00:00 +0700

    create partials hero section

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,17 @@
+<div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-10 col-sm-8 col-lg-6">
+            <img src="img/hero.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500"
+                loading="lazy">
+        </div>
+        <div class="col-lg-6">
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Buy tickets anywhere at any time</h1>
+            <p class="lead">Buy tickets anywhere at any time, without having to queue, explore the various features we
+                provide and enjoy the various promos available.</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <a type="button" href="#" class="btn btn-primary btn-lg px-4 me-md-2">Sigin-Up</a>
+                <a type="button" href="#" class="btn btn-outline-secondary btn-lg px-4">Login</a>
+            </div>
+        </div>
+    </div>
+</div>
