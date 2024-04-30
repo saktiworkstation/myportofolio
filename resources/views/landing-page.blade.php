e6164d439e941d91debdcc100002d5d1504156b7
saktiworkstation <saktiworkstation@gmail.com>
2024-04-30 17:23:54 +0700

    displays the nav, hero and footer sections

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -12,6 +12,11 @@
 <body>
 
     {{-- Section --}}
+    @include('partials.navbar')
+
+    @include('partials.hero')
+
+    @include('partials.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
