7b7029033d359bdae61a4659d321a0eb6ae7f1b7
saktiworkstation <saktiworkstation@gmail.com>
2024-04-30 18:57:50 +0700

    added hero img

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,7 +1,7 @@
 <div class="container col-xxl-8 px-4 py-5">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
         <div class="col-10 col-sm-8 col-lg-6">
-            <img src="img/hero.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500"
+            <img src="img/hero.jpg" class="d-block mx-lg-auto img-fluid" alt="Sakti Kusuma Aji" width="700" height="500"
                 loading="lazy">
         </div>
         <div class="col-lg-6">
