527d6b1c9526658f02d13a0c752951060075d479
saktiworkstation <saktiworkstation@gmail.com>
2024-04-30 23:23:50 +0700

    Create about partials

---
+++ resources/views/partials/about.blade.php
@@ -0,0 +1,10 @@
+<div class="container col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-10 col-sm-8 col-lg-6">
+            Rights
+        </div>
+        <div class="col-lg-6">
+            Left
+        </div>
+    </div>
+</div>
