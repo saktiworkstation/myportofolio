083fe74dffa37c935a141e7ffa795bb0f5243d20
saktiworkstation <saktiworkstation@gmail.com>
2024-05-01 13:43:56 +0700

    Determines the size of the right and left columns

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,10 +1,10 @@
 <div class="bg-info-subtle">
     <div class="container col-xxl-8 px-4 py-5 ">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-10 col-sm-8 col-lg-6">
+            <div class="col-lg-8">
                 Rights
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 Left
             </div>
         </div>
