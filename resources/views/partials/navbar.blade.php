8b62b1739024c747ea6b30738673a5be61eb33cb
saktiworkstation <saktiworkstation@gmail.com>
2024-05-01 14:30:51 +0700

    fixed font size and style

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,16 +1,16 @@
 <div class="container">
     <header
         class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <div class="col-md-3 mb-2 mb-md-0">
+        <div class="col-md-3 mb-2 mb-md-0 fs-5">
             SAKTI KUSUMA AJI
         </div>
 
-        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="/" class="nav-link px-2 link-secondary">Home</a></li>
-            <li><a href="#" class="nav-link px-2">About</a></li>
-            <li><a href="#" class="nav-link px-2">Project</a></li>
-            <li><a href="/" class="nav-link px-2">School</a></li>
-            <li><a href="#" class="nav-link px-2">Contact-Me</a></li>
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 fs-5">
+            <li><a href="/" class="nav-link px-2 ">Home</a></li>
+            <li><a href="#" class="nav-link px-2 link-secondary">About</a></li>
+            <li><a href="#" class="nav-link px-2 link-secondary">Project</a></li>
+            <li><a href="/" class="nav-link px-2 link-secondary">School</a></li>
+            <li><a href="#" class="nav-link px-2 link-secondary">Contact-Me</a></li>
         </ul>
         @auth
             <ul class="navbar-nav ms-auto">
