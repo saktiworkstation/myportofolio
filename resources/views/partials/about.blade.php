d62fb754551f50052c5b4914fc4623c43435a98a
saktiworkstation <saktiworkstation@gmail.com>
2024-05-01 14:16:46 +0700

    Aded some of content

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,14 +1,15 @@
-<div class="bg-info-subtle">
-    <div class="container col-xxl-8 px-4 py-5 ">
-        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-lg-8">
+<div class="bg-body-secondary">
+    <div class="container col-xxl-8 px-4 py-5 text-center">
+        <h1>Hi. I'm Sakti.</h1>
+        <div class="row flex-lg-row-reverse d-flex justify-content-between g-5 py-5">
+            <div class="col-lg-7 text-start">
                 Right
             </div>
             <div class="col-lg-4">
                 <div class="card" style="width: 18rem;">
                     <img src="img/pp.jpg" class="card-img-top" alt="Sakti Kusuma Aji">
-                    <div class="card-header">
-                        Card title
+                    <div class="card-header bg-info-subtle">
+                        Code At Night 🌃
                     </div>
                 </div>
             </div>
