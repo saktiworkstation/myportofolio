11bd15858e6628329443388027e8733f97cbf77d
saktiworkstation <saktiworkstation@gmail.com>
2024-05-01 15:35:54 +0700

    Added content

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -3,7 +3,13 @@
         <h1>Hi. I'm Sakti.</h1>
         <div class="row flex-lg-row-reverse d-flex justify-content-between g-5 py-5">
             <div class="col-lg-7 text-start fs-5">
-                Right
+                <h1>I am a web programmer, currently studying at Amikom University Yogyakarta</h1>
+                <p class="fs-5 my-5">For the past 5+ years, I have been involved in various fields of programming,
+                    including web programming back-end, front-end, and game development. And I am glad to have learned
+                    so much.</p>
+                <p class="fs-5 my-2">Today, I am particularly fond of creating new website and AI projects to further
+                    develop my skills in this field, so that I don't fall too far behind with the continuous emergence
+                    of new technologies.</p>
             </div>
             <div class="col-lg-4">
                 <div class="card" style="width: 18rem;">
