4caa40d0ca894ad88b672d16f71bed20031b7d18
saktiworkstation <saktiworkstation@gmail.com>
2024-05-01 19:18:32 +0700

    Displays project data from the database

--- app/Http/Controllers/LandingPage.php
+++ app/Http/Controllers/LandingPage.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class LandingPage extends Controller
 {
     public function index(){
-        return view('landing-page');
+        return view('landing-page', [
+            'projects' => Project::latest()->get(),
+        ]);
     }
 }
--- resources/views/partials/project.blade.php
+++ resources/views/partials/project.blade.php
@@ -2,19 +2,20 @@
     <h1 class="pb-2 border-bottom">Project</h1>
 
     <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-        <div class="col fs-5">
-            <div class="card text-bg-dark">
-                <img src="img/project.jpg" class="card-img" alt="...">
-                <div class="card-img-overlay">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small><a href="#" class="text-white">Link to this
-                                Project
-                                -></a></small></p>
+        @foreach ($projects as $project)
+            <div class="col fs-5">
+                <div class="card text-bg-dark">
+                    <img src="img/project.jpg" class="card-img" alt="{{ $project->title }}">
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">{{ $project->title }}</h5>
+                        <p class="card-text">{!! $project->description !!}</p>
+                        <p class="card-text"><small><a href="{{ $project->url }}" class="text-white">Link to this
+                                    Project
+                                    -></a></small></p>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 </div>
 </div>
