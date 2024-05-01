bb052f5123c39b80f66771d483a3225c81778a3e
saktiworkstation <saktiworkstation@gmail.com>
2024-05-01 14:19:50 +0700

    Added font

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -7,6 +7,12 @@
     <title>Landing Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Jersey+15&display=swap');
+    </style>
+
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
