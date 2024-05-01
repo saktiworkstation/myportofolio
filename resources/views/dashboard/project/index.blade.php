8f19a0d5f3dd3f34e79f85c800305c111e2b7844
saktiworkstation <saktiworkstation@gmail.com>
2024-05-01 18:16:01 +0700

    create projects page

---
+++ resources/views/dashboard/project/index.blade.php
@@ -0,0 +1,66 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">{{ $title }}</h1>
+    </div>
+
+    @if (session()->has('success'))
+        <div class="alert alert-success alert-dismissible fade show col-lg-8" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
+    <div class="table-responsive col-sd-11">
+        <div class="col-sm-2 mb-3 mb-sm-4">
+            <div class="card border-primary">
+                <div class="card-body">
+                    <a href="/dashboard/projects/create" class="btn btn-primary my-1">Create new Project</a>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4>projects</h4>
+                    <table class="table table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Content</th>
+                                <th scope="col">Published at</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($projects as $project)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $project->title }}</td>
+                                    <td>{!! $project->descriptions !!}</td>
+                                    <td>{{ $project->url }}</td>
+                                    <td>
+                                        <a href="/dashboard/projects/{{ $project->id }}/edit" class="badge bg-warning">
+                                            <span data-feather="edit"></span>
+                                        </a>
+                                        <form action="/dashboard/projects/{{ $project->id }}/delete" method="post"
+                                            class="d-inline">
+                                            @method('delete')
+                                            @csrf
+                                            <button class="badge bg-danger border-0"
+                                                onclick="return confirm('Are you sure want to delete {{ $project->title }}?')">
+                                                <span data-feather="x-circle"></span>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
