f18086a2f7bd70f54c82ec8d14fce6bc3fef888f
saktiworkstation <saktiworkstation@gmail.com>
2024-05-01 13:49:05 +0700

    Added img card to left side

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -2,10 +2,15 @@
     <div class="container col-xxl-8 px-4 py-5 ">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
             <div class="col-lg-8">
-                Rights
+                Right
             </div>
             <div class="col-lg-4">
-                Left
+                <div class="card" style="width: 18rem;">
+                    <img src="img/pp.jpg" class="card-img-top" alt="Sakti Kusuma Aji">
+                    <div class="card-header">
+                        Card title
+                    </div>
+                </div>
             </div>
         </div>
     </div>
